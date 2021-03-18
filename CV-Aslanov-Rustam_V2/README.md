40bc60eb235ee7f9634b748bb21cc0ccf4c8b4c9
Aslanov Rustam <aslanovrg@gmail.com>
2021-03-18 23:30:39 +0200

    new version of CV

---
+++ CV-Aslanov-Rustam_V2/README.md
@@ -0,0 +1,3 @@
+CV Front-End Developer Aslanov Rustam Technologies stack: 'Redux, React, JS,
+Node.js, HTML, Handlebars, CSS, SCSS, REST API, Netlify, GIT, Github, Webpack,
+Parcel, Figma, Trello'
---
+++ CV-Aslanov-Rustam_V2/src/index.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import './sass/utils/variables.scss';
+import './sass/utils/styles.scss';
+import './sass/utils/discharge.scss';
+import './sass/general.scss';
+import './sass/aside.scss';
+import './sass/projects.scss';
+import './js/projects';
